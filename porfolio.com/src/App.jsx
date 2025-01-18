86808384071615ac7a53a2694238ed7c1a3d8408
abdullah-zuzu6 <abdullahsajjad261178@gmail.com>
2025-01-18 20:39:26 +0500

    vite adit

--- porfolio.com/src/App.jsx
+++ porfolio.com/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h3>Han bahi kia haal ha .kadi parh vi lia kr </h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
